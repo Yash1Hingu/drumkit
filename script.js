bc123717ff2683d4e44e486cc7d376bb7eb0e6cc
Hingu Yash <108277321+Yash1Hingu@users.noreply.github.com>
2023-02-06 14:31:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const w = document.getElementById("w");
+const e = document.getElementById("e");
+const r = document.getElementById("r");
+const t = document.getElementById("t");
+const y = document.getElementById("y");
+
+w.addEventListener("keypress",(event) =>{
+    console.log("click");
+})
