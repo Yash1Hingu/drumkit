4c6eec0359571268861bc6d0386408f8981938e4
Hingu Yash <108277321+Yash1Hingu@users.noreply.github.com>
2023-02-08 08:53:30 +0530

    Work in Mobile

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
-document.addEventListener("keydown",(e)=>{
+document.addEventListener("keydown", (e) => {
     var name = e.key;
-    console.log(name);
-    if(name === 'a') {
-        var s1 = new Audio("sounds/tom-1.mp3");
+    if (name === 'a') {
+        let s1 = new Audio("sounds/tom-1.mp3");
         s1.play();
-    } else if(name === "s") {
-        var s2 = new Audio("sounds/tom-2.mp3");
+    } else if (name === "s") {
+        let s2 = new Audio("sounds/tom-2.mp3");
         s2.play();
-    } else if(name === "d") {
-        var s3 = new Audio("sounds/tom-3.mp3");
+    } else if (name === "d") {
+        let s3 = new Audio("sounds/tom-3.mp3");
         s3.play();
-    } else if(name === "f") {
-        var s4 = new Audio("sounds/tom-4.mp3");
+    } else if (name === "f") {
+        let s4 = new Audio("sounds/tom-4.mp3");
         s4.play();
-    } else if(name === "g") {
-        var s5 = new Audio("sounds/snare.mp3");
+    } else if (name === "g") {
+        let s5 = new Audio("sounds/snare.mp3");
         s5.play();
-    } else if(name === "h") {
-        s2 = new Audio("sounds/kick-bass.mp3");
-        s2.play();
-    } else if(name === "l") {
-        var s2 = new Audio("sounds/crash.mp3");
-        s2.play();
+    } else if (name === "h") {
+        let s6 = new Audio("sounds/crash.mp3");
+        s6.play();
+    } else if (name === "l") {
+        let s7 = new Audio("sounds/kick-bass.mp3");
+        s7.play();
     }
 })
+
+
+let drums = document.querySelectorAll('.drum');
+drums.forEach((d) => {
+    d.addEventListener('click', () => {
+        if (d.classList.contains('a')) {
+            let s1 = new Audio("sounds/tom-1.mp3");
+            s1.play();
+        } else if (d.classList.contains('s')) {
+            let s2 = new Audio("sounds/tom-2.mp3");
+            s2.play();
+        } else if (d.classList.contains('d')) {
+            let s3 = new Audio("sounds/tom-3.mp3");
+            s3.play();
+        } else if (d.classList.contains('f')) {
+            let s4 = new Audio("sounds/tom-4.mp3");
+            s4.play();
+        } else if (d.classList.contains('g')) {
+            let s5 = new Audio("sounds/snare.mp3");
+            s5.play();
+        } else if (d.classList.contains('h')) {
+            let s6 = new Audio("sounds/crash.mp3");
+            s6.play();
+        } else if (d.classList.contains('l')) {
+            let s7 = new Audio("sounds/kick-bass.mp3");
+            s7.play();
+        }
+    })
+})
+
